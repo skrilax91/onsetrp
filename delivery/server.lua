ca48bb91b8b5c6cd33f6007186c9e19c2cefddc4
Léo Brossard <leo.skrilax91@gmail.com>
2020-02-26 17:21:22 +0100

    update some files

--- delivery/server.lua
+++ delivery/server.lua
@@ -1,54 +1,15 @@
 local _ = function(k, ...) return ImportPackage("i18n").t(GetPackageName(), k, ...) end
 
-local deliveryNpc = {
-    {
-        location = {-16925, -29058, 2200, -90},
-        spawn = {-17450, -28600, 2060, -90}
-    },
-    {
-        location = {-168301, -41499, 1192, 90},
-        spawn = {-168233, -40914, 1146, 90}
-    },
-    {
-        location = {146585, 211065, 1307, -90},
-        spawn = {145692, 210574, 1307, 0}
-    }
-}
-local deliveryPoint = {
-    {116691, 164243, 3028},
-    {38964, 204516, 550},
-    {182789, 186140, 1203},
-    {211526, 176056, 1209},
-    {42323, 137508, 1569},
-    {122776, 207169, 1282},
-    {209829, 92977, 1312},
-    {176840, 10049, 10370},
-    {198130, -49703, 1109},
-    {130042, 78285, 1566},
-    {203711, 190025, 1312},
-    {170311, -161302, 1242},
-    {152267, -143379, 1242},
-    {-181677, -31627, 1148},
-    {-179804, -66772, 1147},
-    {182881, 148416, 5933}
-}
-
-local LOCATION_PRICE = 2000
-local LOCATION_REFUND_PERCENTAGE = 0.8
-
-local PRICE_DIVIDER = 250
-local PRICE_PER_DIVIDE = 15
-
 local deliveryNpcCached = {}
 local playerDelivery = {}
 
 local trucksOnLocation = {}
 
 AddEvent("OnPackageStart", function()
-    for k, v in pairs(deliveryNpc) do
-        deliveryNpc[k].npc = CreateNPC(deliveryNpc[k].location[1], deliveryNpc[k].location[2], deliveryNpc[k].location[3], deliveryNpc[k].location[4])
-        CreateText3D(_("delivery_job") .. "\n" .. _("press_e"), 18, deliveryNpc[k].location[1], deliveryNpc[k].location[2], deliveryNpc[k].location[3] + 120, 0, 0, 0)
-        table.insert(deliveryNpcCached, deliveryNpc[k].npc)
+    for k, v in pairs(Config.delivery.NPC) do
+        Config.delivery.NPC[k].npc = CreateNPC(Config.delivery.NPC[k].location[1], Config.delivery.NPC[k].location[2], Config.delivery.NPC[k].location[3], Config.delivery.NPC[k].location[4])
+        CreateText3D(_("delivery_job") .. "\n" .. _("press_e"), 18, Config.delivery.NPC[k].location[1], Config.delivery.NPC[k].location[2], Config.delivery.NPC[k].location[3] + 120, 0, 0, 0)
+        table.insert(deliveryNpcCached, Config.delivery.NPC[k].npc)
     end
 end)
 
@@ -59,13 +20,13 @@ AddEvent("OnPlayerQuit", function(player)
 end)
 
 AddEvent("OnPlayerJoin", function(player)
-    CallRemoteEvent(player, "SetupDelivery", deliveryNpcCached, LOCATION_PRICE)
+    CallRemoteEvent(player, "SetupDelivery", deliveryNpcCached, Config.delivery.locationPrice)
 end)
 
 AddRemoteEvent("StartStopDelivery", function(player)
     local nearestDelivery = GetNearestDelivery(player)
-    local useBank = PlayerData[player].bank_balance >= LOCATION_PRICE
-    local useCash = PlayerData[player].inventory['cash'] ~= nil and PlayerData[player].inventory['cash'] >= LOCATION_PRICE
+    local useBank = PlayerData[player].bank_balance >= Config.delivery.locationPrice
+    local useCash = PlayerData[player].inventory['cash'] ~= nil and PlayerData[player].inventory['cash'] >= Config.delivery.locationPrice
     
     if PlayerData[player].job == "" then
         if PlayerData[player].job_vehicle ~= nil then
@@ -86,7 +47,7 @@ AddRemoteEvent("StartStopDelivery", function(player)
             end
             for k, v in pairs(GetAllVehicles()) do
                 local x, y, z = GetVehicleLocation(v)
-                local dist2 = GetDistance3D(deliveryNpc[nearestDelivery].spawn[1], deliveryNpc[nearestDelivery].spawn[2], deliveryNpc[nearestDelivery].spawn[3], x, y, z)
+                local dist2 = GetDistance3D(Config.delivery.NPC[nearestDelivery].spawn[1], Config.delivery.NPC[nearestDelivery].spawn[2], Config.delivery.NPC[nearestDelivery].spawn[3], x, y, z)
                 if dist2 < 500.0 then
                     isSpawnable = false
                     break
@@ -95,15 +56,15 @@ AddRemoteEvent("StartStopDelivery", function(player)
             if isSpawnable then
                 
                 if useBank then
-                    PlayerData[player].bank_balance = PlayerData[player].bank_balance - LOCATION_PRICE
+                    PlayerData[player].bank_balance = PlayerData[player].bank_balance - Config.delivery.locationPrice
                 elseif useCash then
-                    RemovePlayerCash(player, LOCATION_PRICE)
+                    RemovePlayerCash(player, Config.delivery.locationPrice)
                 else
-                    CallRemoteEvent(player, "MakeErrorNotification", _("delivery_not_enough_location_cash", LOCATION_PRICE))
+                    CallRemoteEvent(player, "MakeErrorNotification", _("delivery_not_enough_location_cash", Config.delivery.locationPrice))
                     return
                 end
                 
-                local vehicle = CreateVehicle(24, deliveryNpc[nearestDelivery].spawn[1], deliveryNpc[nearestDelivery].spawn[2], deliveryNpc[nearestDelivery].spawn[3], deliveryNpc[nearestDelivery].spawn[4])
+                local vehicle = CreateVehicle(24, Config.delivery.NPC[nearestDelivery].spawn[1], Config.delivery.NPC[nearestDelivery].spawn[2], Config.delivery.NPC[nearestDelivery].spawn[3], Config.delivery.NPC[nearestDelivery].spawn[4])
                 PlayerData[player].job_vehicle = vehicle
                 CreateVehicleData(player, vehicle, 24)
                 SetVehicleRespawnParams(vehicle, false)
@@ -122,7 +83,7 @@ AddRemoteEvent("StartStopDelivery", function(player)
             local x, y, z = GetVehicleLocation(PlayerData[player].job_vehicle)
             
             local IsNearby = false
-            for k, v in pairs(deliveryNpc) do -- For each location npc
+            for k, v in pairs(Config.delivery.NPC) do -- For each location npc
                 local dist = GetDistance3D(x, y, z, v.location[1], v.location[2], v.location[3])
                 if dist <= 2000 then -- if vehicle is nearby
                     IsNearby = true
@@ -130,7 +91,7 @@ AddRemoteEvent("StartStopDelivery", function(player)
             end
             
             if IsNearby then
-                local refund = LOCATION_PRICE * LOCATION_REFUND_PERCENTAGE
+                local refund = Config.delivery.locationPrice * Config.delivery.locationRefoundPercentage
                 PlayerData[player].bank_balance = PlayerData[player].bank_balance + refund
                 CallRemoteEvent(player, "MakeNotification", _("delivery_location_price_refunded", refund), "linear-gradient(to right, #00b09b, #96c93d)")
             else
@@ -162,12 +123,12 @@ AddRemoteEvent("NextDelivery", function(player)
         return CallRemoteEvent(player, "MakeNotification", _("finish_your_delivery"), "linear-gradient(to right, #ff5f6d, #ffc371)")
     end
     delivery = {}
-    delivery[1] = Random(1, #deliveryPoint)
+    delivery[1] = Random(1, #Config.delivery.point)
     local x, y, z = GetPlayerLocation(player)
-    local dist = GetDistance3D(x, y, z, deliveryPoint[delivery[1]][1], deliveryPoint[delivery[1]][2], deliveryPoint[delivery[1]][3])
-    delivery[2] = ((dist / 100) / PRICE_DIVIDER) * PRICE_PER_DIVIDE
+    local dist = GetDistance3D(x, y, z, Config.delivery.point[delivery[1]][1], Config.delivery.point[delivery[1]][2], Config.delivery.point[delivery[1]][3])
+    delivery[2] = ((dist / 100) / Config.delivery.priceDivider) * Config.delivery.pricePerDivide
     playerDelivery[player] = delivery
-    CallRemoteEvent(player, "ClientCreateWaypoint", _("delivery"), deliveryPoint[delivery[1]][1], deliveryPoint[delivery[1]][2], deliveryPoint[delivery[1]][3])
+    CallRemoteEvent(player, "ClientCreateWaypoint", _("delivery"), Config.delivery.point[delivery[1]][1], Config.delivery.point[delivery[1]][2], Config.delivery.point[delivery[1]][3])
     CallRemoteEvent(player, "MakeNotification", _("new_delivery"), "linear-gradient(to right, #00b09b, #96c93d)")
 end)
 
@@ -181,7 +142,7 @@ AddRemoteEvent("FinishDelivery", function(player)
     
     local x, y, z = GetPlayerLocation(player)
     
-    local dist = GetDistance3D(x, y, z, deliveryPoint[delivery[1]][1], deliveryPoint[delivery[1]][2], deliveryPoint[delivery[1]][3])
+    local dist = GetDistance3D(x, y, z, Config.delivery.point[delivery[1]][1], Config.delivery.point[delivery[1]][2], Config.delivery.point[delivery[1]][3])
     
     if dist < 150.0 then
         if PlayerData[player].job_vehicle ~= GetPlayerVehicle(player) then
@@ -189,9 +150,9 @@ AddRemoteEvent("FinishDelivery", function(player)
             return
         end
         
-        CallRemoteEvent(player, "MakeNotification", _("finished_delivery", math.ceil(delivery[2]) or PRICE_PER_DIVIDE, _("currency")), "linear-gradient(to right, #00b09b, #96c93d)")
+        CallRemoteEvent(player, "MakeNotification", _("finished_delivery", math.ceil(delivery[2]) or Config.delivery.pricePerDivide, _("currency")), "linear-gradient(to right, #00b09b, #96c93d)")
         
-        AddPlayerCash(player, math.ceil(delivery[2]) or PRICE_PER_DIVIDE)
+        AddPlayerCash(player, math.ceil(delivery[2]) or Config.delivery.pricePerDivide)
         playerDelivery[player] = nil
         CallRemoteEvent(player, "ClientDestroyCurrentWaypoint")
     else
@@ -207,7 +168,7 @@ function GetNearestDelivery(player)
         local dist = GetDistance3D(x, y, z, x2, y2, z2)
         
         if dist < 250.0 then
-            for k, i in pairs(deliveryNpc) do
+            for k, i in pairs(Config.delivery.NPC) do
                 if v == i.npc then
                     return k
                 end
